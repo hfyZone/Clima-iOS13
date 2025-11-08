73350e82f987cb0f5e345869e47ea2689d682fdb
hfyZone <1178805691@qq.com>
2025-11-08 22:46:30 +0800

    extension、CodeSnippet

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -8,24 +8,28 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController, UITextFieldDelegate, WeatherManagerDelegate {
+class WeatherViewController: UIViewController {
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
     @IBOutlet weak var searchTextField: UITextField!
-    
+
     var weatherManager = WeatherManager()
-    
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // 将搜索框的动态变化监听委托给viewController
         searchTextField.delegate = self
         weatherManager.delegate = self
-        
+
     }
 
+}
+// MARK: - UITextFieldDelegate
+// The extension helps to organize the code
+// 可以加入code snippet实现自定义简写
+extension WeatherViewController: UITextFieldDelegate {
     @IBAction func searchPressed(_ sender: UIButton) {
         searchTextField.endEditing(true)
     }
@@ -37,31 +41,37 @@ class WeatherViewController: UIViewController, UITextFieldDelegate, WeatherManag
     func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
         if searchTextField.text != "" {
             return true
-        }else{
+        } else {
             textField.placeholder = "Type Something Here!"
             return false
         }
     }
-    
+
     func textFieldDidEndEditing(_ textField: UITextField) {
         if let city = searchTextField.text {
             weatherManager.fetchWather(cityName: city)
         }
-        
+
         searchTextField.text = ""
     }
-    
-    func didUpdateWeather(_ weatherManager: WeatherManager ,weather: WeatherModel){
+}
+
+//MARK: - WeatherManagerDelegate
+
+extension WeatherViewController: WeatherManagerDelegate {
+    func didUpdateWeather(
+        _ weatherManager: WeatherManager,
+        weather: WeatherModel
+    ) {
         DispatchQueue.main.async {
             self.temperatureLabel.text = weather.temperatureString
-            self.conditionImageView.image = UIImage(systemName: weather.conditionName)
+            self.conditionImageView.image = UIImage(
+                systemName: weather.conditionName
+            )
             self.cityLabel.text = weather.cityName
         }
     }
-    func didFailWithError(error: Error){
+    func didFailWithError(error: Error) {
         print(error)
     }
-    
-     
 }
-
--- Clima/Models/WeatherManager.swift
+++ Clima/Models/WeatherManager.swift
@@ -56,3 +56,5 @@ struct WeatherManager  {
     
 
 }
+
+
